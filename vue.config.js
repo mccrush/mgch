dbf1abb3b8c7c19e6df7d1767d759c14c5306451
mccrush <mccrush2027@gmail.com>
2020-12-28 16:05:21 +0700

    Disabled soourceMap

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 // vue.config.js
 module.exports = {
+  productionSourceMap: false,
   publicPath: '/mgch/', // Путь в подкаталог для домена Github page
   outputDir: 'docs' // Имя директории для сборки, для Github page
 }
